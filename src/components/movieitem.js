f1dc01dafd4c8a3432bd5f0b84e66886ca879845
GLEN PAGETT - STUDENT <G00385144@gmit.ie>
2021-11-22 14:03:52 +0000

    first commit

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from  'react-bootstrap/Button';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 // some comments
 class MovieItem extends Component {
+
+constructor(){
+    super();
+    this.DeleteMovie = this.DeleteMovie.bind(this);
+}
+
+DeleteMovie(){
+    axios.delete('http://localhost:4000/api/movies/' +this.props.movie._id)
+    .then(()=>{
+        this.props.ReloadData();
+    })
+    .catch();
+}
+
     render() {
         return (
             <div>
@@ -19,6 +35,7 @@ class MovieItem extends Component {
                         </blockquote>
                     </Card.Body>
 <Link to={"/edit/" +this.props.movie._id} className="btn btn-primary">Edit</Link>
+<Button variant="danger" onClick={this.DeleteMovie}>Delete</Button>
                 </Card>
             </div>
         );
